df7cf803ee05e73f768b0e52949e3218f9aa7162
unknown <olha.klishchevska.ri.2022@lpnu.ua.com>
2022-12-15 13:15:55 +0200

    first commit

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,30 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab09.1(A)/lab09.1(A).cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Student* students = new Student[2];
+
+			students[1].surname = "върї";
+			students[1].course = 3;
+			students[1].physicsGrade = 4;
+			students[1].mathGrade = 3;
+			students[1].ITGrade = 4;
+
+			int GoodGradesCount;
+
+			Assert::AreEqual((students, 2, "върї", 3, GoodGradesCount), GoodGradesCount);
+
+
+		}
+	};
+}
